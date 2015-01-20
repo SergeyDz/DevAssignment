76216e87d87d5eda8de1195887c1364d87310c2c
Sergey Dzyuban <sergey.dzyuban@gmail.com>
2015-01-20 15:16:41 +0200

    Initial solution infratructure for VS2013 (including StyleCop via nuget)

---
+++ Source/SD.CodeProblem.DevAssignment.Contracts/IAccountService.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IAccountService.cs" company="SD">
+//     Copyright (c) 2015. All rights reserved.
+// </copyright>
+// <author>Sergey Dzyuban</author>
+//-----------------------------------------------------------------------
+namespace SD.CodeProblem.DevAssignment.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Account receivable interface definition.
+    /// </summary>
+    public interface IAccountService
+    {
+        /// <summary>
+        /// Get account amount by account id.
+        /// </summary>
+        /// <param name="accountId">Account identifier.</param>
+        /// <returns>Returns account amount value.</returns>
+        double GetAccountAmount(int accountId);
+    }
+}
