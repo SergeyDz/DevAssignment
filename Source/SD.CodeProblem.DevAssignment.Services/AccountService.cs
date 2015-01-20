602973aababb05a0a4dd67e18ee34942b382f675
Sergey Dzyuban <sergey.dzyuban@gmail.com>
2015-01-20 17:28:10 +0200

    AccountService - initial implementation of services added to Services project

--- Source/SD.CodeProblem.DevAssignment.Services/AccountService.cs
+++ Source/SD.CodeProblem.DevAssignment.Services/AccountService.cs
@@ -1,27 +1,31 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="IAccountService.cs" company="SD">
+// <copyright file="AccountService.cs" company="SD">
 //     Copyright (c) 2015. All rights reserved.
 // </copyright>
 // <author>Sergey Dzyuban</author>
 //-----------------------------------------------------------------------
-namespace SD.CodeProblem.DevAssignment.Contracts
+namespace SD.CodeProblem.DevAssignment.Services
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using SD.CodeProblem.DevAssignment.Contracts;
 
     /// <summary>
-    /// Account service interface definition.
+    /// Account-related basic operations service.
     /// </summary>
-    public interface IAccountService
+    public class AccountService : IAccountService
     {
         /// <summary>
-        /// Get account amount by account id.
+        /// Get account amount calculated value by account id.
         /// </summary>
         /// <param name="accountId">Account identifier.</param>
         /// <returns>Returns account amount value.</returns>
-        double GetAccountAmount(int accountId);
+        public double GetAccountAmount(int accountId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
