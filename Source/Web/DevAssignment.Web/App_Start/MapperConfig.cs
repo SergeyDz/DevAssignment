7c8b77f6d87f16427f9a0a973370aaad4e223eef
Sergey Dzyuban <sergey.dzyuban@gmail.com>
2015-02-05 21:48:25 +0200

    ASP.NET MVC 5 site added

---
+++ Source/Web/DevAssignment.Web/App_Start/MapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using SD.CodeProblem.DevAssignment.Services.Mapping;
+
+namespace DevAssignment.WebApi.App_Start
+{
+    public class MapperConfig
+    {
+        public static void ConfigureMappings()
+        {
+            Mapper.Initialize(cfg => cfg.AddProfile<AccountListMappingProfile>());
+            //Mapper.Initialize(cfg => cfg.AddProfile<AccountMappingProfile>());
+        }
+    }
+}
